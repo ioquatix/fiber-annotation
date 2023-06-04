c9bcd35ecd15331bae4179a43d528117709bcaf2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-06-04 23:30:39 +0900

    Documentation.

--- lib/fiber/annotate.rb
+++ lib/fiber/annotate.rb
@@ -6,22 +6,38 @@
 require 'fiber'
 
 class Fiber
+	# A mechanism for annotating fibers.
 	module Annotate
+		# Annotate the current fiber with the given annotation.
+		# @parameter annotation [Object] The annotation to set.
 		def initialize(annotation: nil, **options, &block)
 			@annotation = annotation
 			super(**options, &block)
 		end
 		
+		# Get the current annotation.
+		# @returns [Object] The current annotation.
 		def annotation
 			@annotation
 		end
 		
-		def __annotate__(annotation)
+		# Annotate the current fiber with the given annotation.
+		#
+		# If a block is given, the annotation is set for the duration of the block and then restored.
+		#
+		# This method should only be invoked on the current fiber.
+		#
+		# @parameter annotation [Object] The annotation to set.
+		# @yields {} The block to execute with the given annotation.
+		# @returns [Object] The return value of the block.
+		def annotate(annotation)
+			raise "Cannot annotate a different fiber!" unless Fiber.current == self
+			
 			if block_given?
 				begin
 					current_annotation = @annotation
 					@annotation = annotation
-					yield
+					return yield
 				ensure
 					@annotation = current_annotation
 				end
@@ -35,7 +51,10 @@ end
 unless Fiber.method_defined?(:annotation)
 	Fiber.prepend(Fiber::Annotate)
 	
+	# @scope Fiber
+	# @name self.annotate
+	# Annotate the current fiber with the given annotation.
 	def Fiber.annotate(...)
-		Fiber.current.__annotate__(...)
+		Fiber.current.annotate(...)
 	end
 end
